8067378e493b545d584ec1537366ce4cfd33ae63
Jeff Jones <jeff.jones1@gmail.com>
2016-02-26 16:32:35 -0500

    added response for no photos

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
   $('button').on("click", function() {
+    $('.photo-container').empty();
     var email = $('input').val();
     var url = "https://api.fullcontact.com/v2/person.json?email=" + email + "&apiKey=d5a1cf11e06700a2";
     $.ajax({
       url: url,
       success: function(data) {
         var photos = data.photos;
-        $.each(photos, function(index, photo) {
-          $('.photo-container').append('<img src="' + photo.url + '"' + '>');
-        });
+        if (data.photos !== undefined) {
+          $.each(photos, function(index, photo) {
+            $('.photo-container').append('<img src="' + photo.url + '"' + '>');
+          });
+        } else {
+          $('.photo-container').append('<div class = no-photo><h2> Sorry, we could not find any photos</h2></div>');
+        }
+
       }
-    });
+    })
+
   })
 })
