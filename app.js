87c7ffcf14aa8798ea1ea993819bc7eff19199fa
Jeff Jones <jeff.jones1@gmail.com>
2016-02-26 15:17:27 -0500

    ajax call with full contact working

---
+++ app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('button').on("click", function() {
+    var email = $('input').val();
+    var url = "https://api.fullcontact.com/v2/person.json?email=" + email + "&apiKey=d5a1cf11e06700a2";
+    $.ajax({
+      url: url,
+      success: function(data) {
+        var photos = data.photos;
+        $.each(photos, function(index, photo) {
+          $('.photo-container').append('<img src="' + photo.url + '"' + '>');
+        })
+      }
+    })
+
+  })
+})
